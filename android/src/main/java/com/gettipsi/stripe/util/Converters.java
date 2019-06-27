90a18a36c216a7932b4334e9c45a8428f0597694
OmranElmasry <OmranElmasry@gmail.com>
2019-06-27 17:18:21 +0200

    Update Converters.java

--- android/src/main/java/com/gettipsi/stripe/util/Converters.java
+++ android/src/main/java/com/gettipsi/stripe/util/Converters.java
@@ -1,8 +1,9 @@
 package com.gettipsi.stripe.util;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
